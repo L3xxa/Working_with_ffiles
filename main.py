7d17c235663b66c375571ccf15b80e0a219cf055
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-09 20:17:33 +0200

    Complete ex_4

---
+++ main.py
@@ -0,0 +1,13 @@
+with open('origin.txt', 'r') as file:
+    content = file.readlines()
+
+insert_index = len(content)
+for i in range(len(content) - 1, -1, -1):
+    if ',' not in content[i]:
+        insert_index = i + 1
+        break
+
+content.insert(insert_index, '************\n')
+
+with open('overwrite.txt', 'w') as output_file:
+    output_file.writelines(content)
